e56228cfe93947a8698f180c134bf89b1f43f4a6
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2024-01-23 16:47:05 -0500

    Revert "RaaS SDK updated"
    This reverts commit 9eb08793bd0e081a7c83ff8b42e1576adc8a741d.

--- raassdkpy/models/raas_pre_quote_request.py
+++ raassdkpy/models/raas_pre_quote_request.py
@@ -18,88 +18,106 @@ import re  # noqa: F401
 import json
 
 
-from typing import Any, Optional, Union
-from pydantic import BaseModel, Field, StrictBool, StrictFloat, StrictInt, StrictStr, validator
+from typing import Any, ClassVar, Dict, List, Optional, Union
+from pydantic import BaseModel, StrictBool, StrictFloat, StrictInt, StrictStr, field_validator
+from pydantic import Field
+try:
+    from typing import Self
+except ImportError:
+    from typing_extensions import Self
 
 class RaasPreQuoteRequest(BaseModel):
     """
     RaasPreQuoteRequest
-    """
-    recipient_id: Optional[StrictStr] = Field(None, alias="RecipientId")
-    subscriber_id: Optional[StrictStr] = Field(None, alias="SubscriberId")
-    destination_payment_method: Optional[Any] = Field(..., alias="DestinationPaymentMethod")
-    sender_country_code: StrictStr = Field(..., alias="SenderCountryCode")
-    is_sender_amount: StrictBool = Field(..., alias="IsSenderAmount")
-    amount: Union[StrictFloat, StrictInt] = Field(..., alias="Amount")
-    operation_type: StrictStr = Field(..., alias="OperationType")
-    product_type: StrictStr = Field(..., alias="ProductType")
-    tennant_fee: Optional[Union[StrictFloat, StrictInt]] = Field(None, alias="TennantFee")
-    __properties = ["RecipientId", "SubscriberId", "DestinationPaymentMethod", "SenderCountryCode", "IsSenderAmount", "Amount", "OperationType", "ProductType", "TennantFee"]
+    """ # noqa: E501
+    recipient_id: Optional[StrictStr] = Field(default=None, alias="RecipientId")
+    subscriber_id: Optional[StrictStr] = Field(default=None, alias="SubscriberId")
+    destination_payment_method: Optional[Any] = Field(alias="DestinationPaymentMethod")
+    sender_country_code: StrictStr = Field(alias="SenderCountryCode")
+    is_sender_amount: StrictBool = Field(alias="IsSenderAmount")
+    amount: Union[StrictFloat, StrictInt] = Field(alias="Amount")
+    operation_type: StrictStr = Field(alias="OperationType")
+    product_type: StrictStr = Field(alias="ProductType")
+    tennant_fee: Optional[Union[StrictFloat, StrictInt]] = Field(default=None, alias="TennantFee")
+    __properties: ClassVar[List[str]] = ["RecipientId", "SubscriberId", "DestinationPaymentMethod", "SenderCountryCode", "IsSenderAmount", "Amount", "OperationType", "ProductType", "TennantFee"]
 
-    @validator('operation_type')
+    @field_validator('operation_type')
     def operation_type_validate_enum(cls, value):
         """Validates the enum"""
         if value not in ('SendFunds', 'RequestFunds', 'WalletLoad', 'WalletTransferOut', 'TopUp', 'RequestTopUp', 'SendFundsVirtualAgent', 'RequestFundsVirtualAgent', 'StorePayment'):
             raise ValueError("must be one of enum values ('SendFunds', 'RequestFunds', 'WalletLoad', 'WalletTransferOut', 'TopUp', 'RequestTopUp', 'SendFundsVirtualAgent', 'RequestFundsVirtualAgent', 'StorePayment')")
         return value
 
-    @validator('product_type')
+    @field_validator('product_type')
     def product_type_validate_enum(cls, value):
         """Validates the enum"""
         if value not in ('Fund', 'Airtime', 'Loan', 'BillPay', 'NotApplicable', 'VirtualAgent', 'Wallet', 'StorePayment', 'NumiService'):
             raise ValueError("must be one of enum values ('Fund', 'Airtime', 'Loan', 'BillPay', 'NotApplicable', 'VirtualAgent', 'Wallet', 'StorePayment', 'NumiService')")
         return value
 
-    class Config:
-        """Pydantic configuration"""
-        allow_population_by_field_name = True
-        validate_assignment = True
+    model_config = {
+        "populate_by_name": True,
+        "validate_assignment": True,
+        "protected_namespaces": (),
+    }
+
 
     def to_str(self) -> str:
         """Returns the string representation of the model using alias"""
-        return pprint.pformat(self.dict(by_alias=True))
+        return pprint.pformat(self.model_dump(by_alias=True))
 
     def to_json(self) -> str:
         """Returns the JSON representation of the model using alias"""
+        # TODO: pydantic v2: use .model_dump_json(by_alias=True, exclude_unset=True) instead
         return json.dumps(self.to_dict())
 
     @classmethod
-    def from_json(cls, json_str: str) -> RaasPreQuoteRequest:
+    def from_json(cls, json_str: str) -> Self:
         """Create an instance of RaasPreQuoteRequest from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
-    def to_dict(self):
-        """Returns the dictionary representation of the model using alias"""
-        _dict = self.dict(by_alias=True,
-                          exclude={
-                          },
-                          exclude_none=True)
+    def to_dict(self) -> Dict[str, Any]:
+        """Return the dictionary representation of the model using alias.
+
+        This has the following differences from calling pydantic's
+        `self.model_dump(by_alias=True)`:
+
+        * `None` is only added to the output dict for nullable fields that
+          were set at model initialization. Other fields with value `None`
+          are ignored.
+        """
+        _dict = self.model_dump(
+            by_alias=True,
+            exclude={
+            },
+            exclude_none=True,
+        )
         # set to None if destination_payment_method (nullable) is None
-        # and __fields_set__ contains the field
-        if self.destination_payment_method is None and "destination_payment_method" in self.__fields_set__:
+        # and model_fields_set contains the field
+        if self.destination_payment_method is None and "destination_payment_method" in self.model_fields_set:
             _dict['DestinationPaymentMethod'] = None
 
         return _dict
 
     @classmethod
-    def from_dict(cls, obj: dict) -> RaasPreQuoteRequest:
+    def from_dict(cls, obj: Dict) -> Self:
         """Create an instance of RaasPreQuoteRequest from a dict"""
         if obj is None:
             return None
 
         if not isinstance(obj, dict):
-            return RaasPreQuoteRequest.parse_obj(obj)
+            return cls.model_validate(obj)
 
-        _obj = RaasPreQuoteRequest.parse_obj({
-            "recipient_id": obj.get("RecipientId"),
-            "subscriber_id": obj.get("SubscriberId"),
-            "destination_payment_method": obj.get("DestinationPaymentMethod"),
-            "sender_country_code": obj.get("SenderCountryCode"),
-            "is_sender_amount": obj.get("IsSenderAmount"),
-            "amount": obj.get("Amount"),
-            "operation_type": obj.get("OperationType"),
-            "product_type": obj.get("ProductType"),
-            "tennant_fee": obj.get("TennantFee")
+        _obj = cls.model_validate({
+            "RecipientId": obj.get("RecipientId"),
+            "SubscriberId": obj.get("SubscriberId"),
+            "DestinationPaymentMethod": obj.get("DestinationPaymentMethod"),
+            "SenderCountryCode": obj.get("SenderCountryCode"),
+            "IsSenderAmount": obj.get("IsSenderAmount"),
+            "Amount": obj.get("Amount"),
+            "OperationType": obj.get("OperationType"),
+            "ProductType": obj.get("ProductType"),
+            "TennantFee": obj.get("TennantFee")
         })
         return _obj
 
