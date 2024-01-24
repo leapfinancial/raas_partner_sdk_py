82d52f5d915e00c1d8992bb27afbc0e2cde1bb65
Andres Pinto <andres.pinto2203@gmail.com>
2024-01-24 17:31:23 -0400

    tests

--- raassdkpy/models/is_phone_available_response.py
+++ raassdkpy/models/is_phone_available_response.py
@@ -68,7 +68,7 @@ class IsPhoneAvailableResponse(BaseModel):
         _obj = IsPhoneAvailableResponse.parse_obj({
             "available": obj.get("available"),
             "verified": obj.get("verified"),
-            "has_pincode": obj.get("has_pincode")
+            "has_pincode": obj.get("hasPincode")
         })
         return _obj
 
